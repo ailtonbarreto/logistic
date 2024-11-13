b1e7941592fa76fdf7697aa1e9d8b20245b20f24
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-11-13 19:22:07 -0300

    j

--- dash.py
+++ dash.py
@@ -295,7 +295,7 @@ st.markdown(borda, unsafe_allow_html=True)
 
 botao = """
             <style>
-            [data-testid="stFullScreenFrame"]
+            [data-testid="StyledFullScreenButton"]
             {
             visibility: hidden;
             }
